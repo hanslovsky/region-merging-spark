b268f63328edf2e64983679643fff485c6ee7ed6
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-09-21 10:19:48 -0400

    Add support for edges pointing in backward direction

--- src/main/java/de/hanslovsky/regionmerging/DataPreparation.java
+++ src/main/java/de/hanslovsky/regionmerging/DataPreparation.java
@@ -328,6 +328,187 @@ public class DataPreparation
 		return new Tuple2<>( blockPosition, new Tuple2<>( labels, affinities ) );
 	}
 
+	public static < I extends IntegerType< I >, R extends RealType< R > > void addEdgesPointingBackwards(
+			final RandomAccessible< I > labels,
+			final RandomAccessible< R > affinities,
+			final Interval interval,
+			final int d,
+			final TLongObjectHashMap< TLongIntHashMap > nodeEdgeMap,
+			final EdgeCreator creator,
+			final EdgeMerger merger,
+			final Edge e,
+			final Edge dummy )
+	{
+		final IntervalView< I > innerLabels = Views.interval( labels, interval );
+		final Cursor< R > affinitiesCursor = Views.interval( affinities, interval ).cursor();
+		final Cursor< I > labelsCursor = innerLabels.cursor();
+		final RandomAccess< I > labelsAccess = labels.randomAccess();
+		while ( affinitiesCursor.hasNext() )
+		{
+			final double affinity = affinitiesCursor.next().getRealDouble();
+			labelsCursor.fwd();
+			if ( !Double.isNaN( affinity ) )
+			{
+				labelsAccess.setPosition( labelsCursor );
+				labelsAccess.bck( d );
+				final long l1 = labelsCursor.get().getIntegerLong();
+				final long l2 = labelsAccess.get().getIntegerLong();
+//				System.out.println( "WUT " + affinity + " " + l1 + " " + l2 );
+
+//				if ( Arrays.stream( Intervals.minAsLongArray( interval ) ).filter( m -> m > 0 ).toArray().length == 0 )
+//					System.out.println(
+//							Arrays.toString( Intervals.minAsLongArray( interval ) ) + " " +
+//									Arrays.toString( Intervals.maxAsLongArray( interval ) ) + " " +
+//									new Point( labelsCursor ) + " " + new Point( labelsAccess ) + " " + labelsCursor.get() + " " + labelsAccess.get() );
+				if ( l1 != l2 && l1 > 0 && l2 > 0 )
+					addEdge( labelsCursor.get().getIntegerLong(), labelsAccess.get().getIntegerLong(), affinity, nodeEdgeMap, creator, merger, e, dummy );
+			}
+		}
+	}
+
+	public static < I extends IntegerType< I >, R extends RealType< R > > TIntHashSet addEdgesAcrossBorderPointingBackwards(
+			final RandomAccessible< I > labels,
+			final RandomAccessible< R > affinities,
+			final Interval interval,
+			final int d,
+			final TLongObjectHashMap< TLongIntHashMap > nodeEdgeMap,
+			final EdgeCreator creator,
+			final EdgeMerger merger,
+			final Edge e,
+			final Edge dummy )
+	{
+		final TIntHashSet nonContractingEdges = new TIntHashSet();
+		final IntervalView< I > innerLabels = Views.interval( labels, interval );
+		final Cursor< R > affinitiesCursor = Views.interval( affinities, interval ).cursor();
+		final Cursor< I > labelsCursor = innerLabels.cursor();
+		final RandomAccess< I > labelsAccess = labels.randomAccess();
+//		System.out.println( "presenting " + Arrays.toString( Intervals.minAsLongArray( interval ) ) );
+//		for ( long y = interval.min( 1 ); y <= interval.max( 1 ); ++y )
+//		{
+//			labelsAccess.setPosition( y, 1 );
+//			for ( long x = interval.min( 0 ); x <= interval.max( 0 ); ++x )
+//			{
+//				labelsAccess.setPosition( x, 0 );
+//				System.out.print( labelsAccess.get() + " " );
+//
+//			}
+//			System.out.println( "" );
+//		}
+//		System.out.println( "Image presented by" );
+//		System.out.println( Arrays.toString( Intervals.minAsLongArray( interval ) ) + " " + Arrays.toString( Intervals.maxAsLongArray( interval ) ) + " WEEEET" );
+		while ( affinitiesCursor.hasNext() )
+		{
+			final double affinity = affinitiesCursor.next().getRealDouble();
+//			System.out.println( "WABABABABA " + affinity + new Point( affinitiesCursor ) );
+			labelsCursor.fwd();
+			if ( !Double.isNaN( affinity ) )
+			{
+				labelsAccess.setPosition( labelsCursor );
+				labelsAccess.bck( d );
+				final long l1 = labelsCursor.get().getIntegerLong();
+				final long l2 = labelsAccess.get().getIntegerLong();
+				if ( l1 != l2 && l1 > 0 && l2 > 0 )
+				{
+					final int edgeIndex = addEdge( labelsCursor.get().getIntegerLong(), labelsAccess.get().getIntegerLong(), affinity, nodeEdgeMap, creator, merger, e, dummy );
+//				if ( interval.min( 0 ) == 3 && interval.min( 1 ) == 4 )
+//					System.out.println( "WAS DA LOS EY? " +
+//							Arrays.toString( Intervals.minAsLongArray( interval ) ) + " " +
+//							Arrays.toString( Intervals.maxAsLongArray( interval ) ) + " " +
+//							new Point( labelsCursor ) + " " + new Point( labelsAccess ) + " " + labelsCursor.get() + " " + labelsAccess.get() + " " + edgeIndex );
+					nonContractingEdges.add( edgeIndex );
+				}
+			}
+		}
+		return nonContractingEdges;
+	}
+
+	public static < I extends IntegerType< I > & NativeType< I >, R extends RealType< R > & NativeType< R >, LA extends ArrayDataAccess< LA >, AA extends ArrayDataAccess< AA > >
+	JavaPairRDD< HashWrapper< long[] >, Data > createGraphPointingBackwards(
+			final JavaSparkContext sc,
+			final Loader< I, R, LA, AA > loader,
+			final EdgeCreator creator,
+			final EdgeMerger merger )
+	{
+		final long[] dimensions = loader.dimensions();
+		final int nDim = dimensions.length;
+		final int[] blockSize = loader.blockSize();
+
+		final List< HashWrapper< long[] > > blocks = collectAllOffsets( dimensions, blockSize, offset -> HashWrapper.longArray( IntStream.range( 0, nDim ).mapToLong( d -> offset[ d ] / blockSize[ d ] ).toArray() ) );
+
+		final JavaRDD< HashWrapper< long[] > > blocksRdd = sc.parallelize( blocks );
+
+		final Broadcast< Loader< I, R, LA, AA > > loaderBC = sc.broadcast( loader );
+
+		final JavaPairRDD< HashWrapper< long[] >, Tuple2< RandomAccessibleInterval< I >, RandomAccessibleInterval< R > > > data = blocksRdd.mapToPair( blockPosition -> createRAIs( blockPosition, loaderBC ) );
+
+//		System.out.println( "after creating RAIS" + data.count() );
+
+		final JavaPairRDD< HashWrapper< long[] >, Data > graphs = data.mapToPair( positionAndData -> {
+			final Loader< I, R, LA, AA > ldr = loaderBC.getValue();
+			final long[] dim = ldr.dimensions();
+			final long[] position = positionAndData._1().getData();
+			final int[] bs = ldr.blockSize();
+
+			final long[] min = IntStream.range( 0, nDim ).mapToLong( d -> Math.max( position[ d ] * bs[ d ], 0 ) ).toArray();
+			final long[] max = IntStream.range( 0, nDim ).mapToLong( d -> Math.min( min[ d ] + bs[ d ], dim[ d ] ) - 1 ).toArray();
+			final long[] innerMax = IntStream.range( 0, nDim ).mapToLong( d -> Math.min( min[ d ] + bs[ d ] - 1, dim[ d ] - 1 ) - 1 ).toArray();
+
+			final RandomAccessibleInterval< I > labels = positionAndData._2()._1();
+			final RandomAccessibleInterval< R > affinities = positionAndData._2()._2();
+
+			final TDoubleArrayList edgeStore = new TDoubleArrayList();
+			final Edge e = new Edge( edgeStore, creator.dataSize() );
+			final Edge dummy = new Edge( new TDoubleArrayList(), creator.dataSize() );
+			final TLongObjectHashMap< TLongIntHashMap > nodeEdgeMap = new TLongObjectHashMap<>();
+			final HashMap< HashWrapper< long[] >, TIntHashSet > nonContractingEdges = new HashMap<>();
+			final TLongLongHashMap counts = new TLongLongHashMap();
+
+			for ( int d = 0; d < nDim; ++d )
+			{
+				final long[] currentMax = max.clone();
+				currentMax[ d ] = innerMax[ d ];
+				final IntervalView< R > hs = Views.hyperSlice( affinities, nDim, ( long ) d );
+				addEdgesPointingBackwards( labels, hs, new FinalInterval( min, currentMax ), d, nodeEdgeMap, creator, merger, e, dummy );
+
+				if ( min[ d ] > 0 )
+				{
+					final long[] lowerPos = position.clone();
+					lowerPos[ d ] -= 1;
+					final long[] mm = min.clone();
+					final long[] MM = max.clone();
+					mm[ d ] -= 1;
+					MM[ d ] = mm[ d ];
+//					System.out.println( "Adding lower: " + Arrays.toString( mm ) + " " + Arrays.toString( MM ) + " " + Arrays.toString( position ) + Arrays.toString( min ) + " " + d );
+					final TIntHashSet edgeIndices = addEdgesAcrossBorderPointingBackwards( labels, hs, new FinalInterval( mm, MM ), d, nodeEdgeMap, creator, merger, e, dummy );
+					nonContractingEdges.put( HashWrapper.longArray( lowerPos ), edgeIndices );
+				}
+
+				if ( max[ d ] < dim[ d ] - 1 )
+				{
+					final long[] upperPos = position.clone();
+					upperPos[ d ] += 1;
+//					System.out.println( "WUT WUT! " + Arrays.toString( min ) + " " + Arrays.toString( max ) );
+					final long[] mm = min.clone();
+					final long[] MM = max.clone();
+					mm[ d ] = MM[ d ];
+//					System.out.println( "Adding upper: " + Arrays.toString( mm ) + " " + Arrays.toString( MM ) + " " + Arrays.toString( position ) + Arrays.toString( min ) + "  " + d );
+					final TIntHashSet edgeIndices = addEdgesAcrossBorderPointingBackwards( labels, hs, new FinalInterval( mm, MM ), d, nodeEdgeMap, creator, merger, e, dummy );
+//					System.out.println( "Adding edge indices! " + edgeIndices );
+					nonContractingEdges.put( HashWrapper.longArray( upperPos ), edgeIndices );
+				}
+
+			}
+//			System.out.println( "Putting " + Arrays.toString( min ) + " " + Arrays.toString( max ) + " " + nonContractingEdges );
+			final Data dat = new Data( edgeStore, nonContractingEdges, counts );
+//			System.out.println( "Looking " + Arrays.toString( min ) + " " + Arrays.toString( max ) + " " + dat.nonContractingEdges() );
+
+			return new Tuple2<>( positionAndData._1(), dat );
+		} );
+
+		return setValidAndStale( graphs, merger.dataSize() );
+
+	}
+
 	public static < K > JavaPairRDD< K, Data > setValidAndStale( final JavaPairRDD< K, Data > input, final int dataSize )
 	{
 		return input.mapValues( data -> {
